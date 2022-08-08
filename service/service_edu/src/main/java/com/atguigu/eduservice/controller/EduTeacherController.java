5c26b4f106c1aa6b078d619cd3448d7049190f78
username <1182243538@qq.com>
2022-08-08 14:33:23 +0800

    git

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author edu
+ * @since 2022-08-08
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.service;
+
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author edu
+ * @since 2022-08-08
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
