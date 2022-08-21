7d8627e13c744134585899f2429690a3d23b6a8b
LXX <15996160048@163.com>
2022-08-21 23:06:35 +0800

    删除基础服务中的冗余文件，调整文件路径与名称

--- common/service_base/src/main/java/com/gzuniversity/servicebase/handler/MyMetaObjectHandler.java
+++ common/service_base/src/main/java/com/gzuniversity/servicebase/handler/MyMetaObjectHandler.java
@@ -1,4 +1,4 @@
-package com.atguigu.servicebase.handler;
+package com.gzuniversity.servicebase.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
